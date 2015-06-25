86c232a14a1d422f0cf263456d21f1d010592b52
YaguangZhu <yaguangzhu@YaguangZhudeMacBook-Pro.local>
2015-06-25 10:53:25 +0800

    Initial Commit

---
+++ test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  test
+//
+//  Created by YaguangZhu on 15/6/25.
+//  Copyright (c) 2015年 YaguangZhu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
